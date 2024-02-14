ef023c47e3aab785d8da7b594dff4840b16c766d
Pratham-Anand <anandpratham13@gmail.com>
2024-02-14 10:52:27 +0530

    Done till custom permissions
    Done till custom permissions

--- CarDekho/CarDekho_app/api_file/serializers.py
+++ CarDekho/CarDekho_app/api_file/serializers.py
@@ -1,30 +1,49 @@
 from rest_framework import serializers
-from ..models import Carlist
+from ..models import Carlist,Showroomlist,Review
 
 def alphanumberic(value):
     if not str(value).isalnum():
         raise serializers.ValidationError("Only alphanumeric characters are allowed")
-
-
-class CarSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField()
-    description=serializers.CharField()
-    active=serializers.BooleanField(read_only=True)
-    chassisnumber=serializers.CharField(validators=[alphanumberic])
-    price=serializers.DecimalField(max_digits=9,decimal_places=2)
-
-    def create(self,validated_data):
-        return Carlist.objects.create(**validated_data)
     
-    def update(self,instance,validated_data):
-        instance.name=validated_data.get('name',instance.name)
-        instance.description=validated_data.get('description',instance.description)
-        instance.active=validated_data.get('active',instance.active)
-        instance.chassisnumber=validated_data.get('chassisnumber',instance.chassisnumber)
-        instance.price=validated_data.get('price',instance.price)
-        instance.save()
-        return instance
+class ReviewSerializers(serializers.ModelSerializer):
+    apiuser=serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model=Review
+        exclude=('car',)
+        # fields="__all__"
+    
+
+class CarSerializer(serializers.ModelSerializer):
+    discounted_price=serializers.SerializerMethodField()
+    chassisnumber=serializers.CharField(validators=[alphanumberic])
+    Reviews=ReviewSerializers(many=True,read_only=True)
+    class Meta:
+        model=Carlist
+        fields="__all__"
+    # id=serializers.IntegerField(read_only=True)
+    # name=serializers.CharField()
+    # description=serializers.CharField()
+    # active=serializers.BooleanField(read_only=True)
+    # chassisnumber=serializers.CharField(validators=[alphanumberic])
+    # price=serializers.DecimalField(max_digits=9,decimal_places=2)
+
+    # def create(self,validated_data):
+    #     return Carlist.objects.create(**validated_data)
+    
+    # def update(self,instance,validated_data):
+    #     instance.name=validated_data.get('name',instance.name)
+    #     instance.description=validated_data.get('description',instance.description)
+    #     instance.active=validated_data.get('active',instance.active)
+    #     instance.chassisnumber=validated_data.get('chassisnumber',instance.chassisnumber)
+    #     instance.price=validated_data.get('price',instance.price)
+    #     instance.save()
+    #     return instance
+        
+    def get_discounted_price(self,object):
+        if object.price is not None:
+            discountprice=object.price - 5000
+            return discountprice
+        return None
     
     def validate_price(self,value):
         if value <=20000.00:
@@ -35,5 +54,21 @@ class CarSerializer(serializers.Serializer):
         if data['name']==data['description']:
             raise serializers.ValidationError("Name and description must be different")
         return data
+    
+class ShowroomSerializer(serializers.ModelSerializer):
+    # Showrooms=CarSerializer(many=True,read_only=True)
+    # Showrooms=serializers.StringRelatedField(many=True)
+    # Showrooms=serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    Showrooms=serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='car_detail',
+    )
+    class Meta:
+        model=Showroomlist
+        fields="__all__"
+        
+
+    
 
     
--- CarDekho/CarDekho_app/views.py
+++ CarDekho/CarDekho_app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from .models import Carlist
+from .models import Carlist,Showroomlist,Review
 from django.http import JsonResponse
-from .api_file.serializers import CarSerializer
+# from django.contrib.auth import logout
+from django.shortcuts import get_object_or_404
+from .api_file.serializers import CarSerializer,ShowroomSerializer,ReviewSerializers
+from .api_file.permissions import AdminOrReadOnlyPermission,ReviewUserorReadonlypermission
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication,SessionAuthentication
+from rest_framework.permissions import IsAdminUser,IsAuthenticated,DjangoModelPermissions
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
@@ -24,7 +35,140 @@ from rest_framework import status
 #     }
 #     return JsonResponse(data)
 
-@api_view(['GET','POST'])
+
+# class ReviewDetail(mixins.RetrieveModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+#     queryset=Review.objects.all()
+#     serializer_class=ReviewSerializers
+
+#     def get(self,request,*args,**kwargs):
+#         return self.retrieve(request,*args,**kwargs)
+    
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+    
+# class ReviewList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView): #Gneneric View and Mixins
+#     queryset=Review.objects.all()
+#     serializer_class=ReviewSerializers
+#     authentication_classes=[SessionAuthentication]
+#     permission_classes=[DjangoModelPermissions]
+    
+#     def get(self,request,*args,**kwargs):
+#         return self.list(request,*args,**kwargs)
+    
+#     def post(self,request,*args,**kwargs):
+#         return self.create(request,*args,**kwargs)
+
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class=ReviewSerializers
+
+    def get_queryset(self):
+        return Review.objects.all()
+
+    def perform_create(self, serializer):
+        pk=self.kwargs['pk']
+        cars=Carlist.objects.get(pk=pk)
+        useredit=self.request.user
+        Review_queryset=Review.objects.filter(car=cars,apiuser=useredit)
+        if Review_queryset.exists():
+            raise ValidationError("You have already viewed this car")
+        serializer.save(car=cars)
+
+class ReviewList(generics.ListAPIView):   #Generic View
+    # queryset=Review.objects.all()
+    serializer_class=ReviewSerializers
+    def get_queryset(self):
+        pk=self.kwargs['pk']
+        return Review.objects.filter(car=pk)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):  #Generic View
+    queryset=Review.objects.all()
+    serializer_class=ReviewSerializers
+    permission_classes=[ReviewUserorReadonlypermission]
+
+# class Showroom_Viewset(viewsets.ReadOnlyModelViewSet):    ##Model View set
+#     queryset = Showroomlist.objects.all()
+    # serializer_class = ShowroomSerializer
+    
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+
+
+class Showroom_Viewset(viewsets.ViewSet):       #ViewSet and Routers
+    def list(self, request):
+        queryset = Showroomlist.objects.all()
+        serializer = ShowroomSerializer(queryset, many=True,context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Showroomlist.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = ShowroomSerializer(user,context={'request': request})
+        return Response(serializer.data)
+    
+    def create(self,request):
+        serializer=ShowroomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def destroy(self,request,pk):
+        showroom=Showroomlist.objects.get(pk=pk)
+        showroom.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+class Showroom_View(APIView):
+    # authentication_classes=[BasicAuthentication]
+    # permission_classes=[IsAuthenticated]
+    # permission_classes=[AllowAny]
+    # permission_classes=[IsAdminUser]
+    # authentication_classes=[SessionAuthentication]
+    # permission_classes=[IsAuthenticated]
+    
+    def get(self,request):
+        showroom=Showroomlist.objects.all()
+        serializer=ShowroomSerializer(showroom,many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer=ShowroomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+class Showroom_Details(APIView):
+    def get(self,request,pk):
+        try:
+            showroom=Showroomlist.objects.get(pk=pk)
+        except Showroomlist.DoesNotExist:
+            return Response({'Error':'Showroom not found'},status=status.HTTP_404_NOT_FOUND)
+        
+        serializer=ShowroomSerializer(showroom,context={'request': request})
+        return Response(serializer.data)
+    
+        
+    def put(self,request,pk):
+        showroom=Showroomlist.objects.get(pk=pk)
+        serializer=ShowroomSerializer(showroom,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,statusstatus=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self,request,pk):
+        showroom=Showroomlist.objects.get(pk=pk)
+        showroom.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+@api_view(['GET','POST'])    #Api view (decorators)
 def car_list_view(request):
     if request.method=='GET':
         car=Carlist.objects.all()
